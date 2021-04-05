15885d4e2c7b7bbb4d573ea88be99e2be41b929b
Jeff Hughes <jeff.hughes@gmail.com>
2021-04-05 18:05:16 -0400

    Fix nginx errors about multiple bindings to same port

--- ssl_renew.sh
+++ ssl_renew.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 COMPOSE="/usr/local/bin/docker-compose -f compose.common.yml -f compose.prod.yml"
+DOCKER="/usr/bin/docker"
 
 cd /home/jeff/portertree/
 $COMPOSE run certbot renew && $COMPOSE exec -d -T app nginx -s reload
+$DOCKER system prune -f
