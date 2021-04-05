a01bc2e717b24b3bdf26c9abec998ec43f0932f0
Jeff Hughes <jeff.hughes@gmail.com>
2021-04-05 15:52:24 -0400

    Add some extra DB constraints

--- db/load_data.sql
+++ db/load_data.sql
@@ -10,11 +10,11 @@ CREATE TABLE people (
     pref_name CHAR(2),
     gender CHAR(1),
     birth_month TEXT,
-    birth_day INTEGER,
+    birth_day INTEGER CHECK (birth_day >= 0 AND birth_day <= 31),
     birth_year TEXT,
     birth_place TEXT,
     death_month TEXT,
-    death_day INTEGER,
+    death_day INTEGER CHECK (death_day >= 0 AND death_day <= 31),
     death_year TEXT,
     death_place TEXT,
     buried TEXT,
@@ -25,26 +25,26 @@ CREATE TABLE marriages (
     id SERIAL PRIMARY KEY,
     pid1 TEXT NOT NULL,
     pid2 TEXT NOT NULL,
-    marriage_order INTEGER,
+    marriage_order INTEGER CHECK marriage_order > 0,
     married_month TEXT,
-    married_day INTEGER,
+    married_day INTEGER CHECK (married_day >= 0 AND married_day <= 31),
     married_year TEXT,
     married_place TEXT,
     divorced BOOLEAN,
     divorced_month TEXT,
-    divorced_day INTEGER,
+    divorced_day INTEGER CHECK (divorced_day >= 0 AND divorced_day <= 31),
     divorced_year TEXT,
-    FOREIGN KEY (pid1) REFERENCES people (id),
-    FOREIGN KEY (pid2) REFERENCES people (id)
+    FOREIGN KEY (pid1) REFERENCES people (id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (pid2) REFERENCES people (id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE children (
     id SERIAL PRIMARY KEY,
     pid TEXT NOT NULL,
     cid TEXT NOT NULL,
-    birth_order INTEGER,
-    FOREIGN KEY (pid) REFERENCES people (id),
-    FOREIGN KEY (cid) REFERENCES people (id)
+    birth_order INTEGER CHECK birth_order > 0,
+    FOREIGN KEY (pid) REFERENCES people (id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (cid) REFERENCES people (id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 COPY people (id, print_id, in_tree, first_name, nickname, middle_name1, middle_name2, last_name, pref_name, gender, birth_month, birth_day, birth_year, birth_place, death_month, death_day, death_year, death_place, buried, additional_notes) FROM '/data_imports/people.csv' CSV HEADER;
