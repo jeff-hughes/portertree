79a1bc10e8c9119de77fe24debd565475977fc5d
Jeff Hughes <jeff.hughes@gmail.com>
2021-04-06 07:38:48 -0400

    Pull out helper functions into a utils module

--- app/app/main.py
+++ app/app/main.py
@@ -1,8 +1,5 @@
-from datetime import datetime
 import json
 import os
-import shutil
-import tempfile
 from urllib.parse import urlparse, urljoin
 
 from flask import abort, Flask, flash, redirect, render_template, request, url_for
@@ -11,12 +8,7 @@ from flask_mailman import Mail, EmailMessage
 
 from auth import User, hash_pass
 from db import DBConnect, DBEntry, DBEntryType, PERSON_COLS, MARRIAGE_COLS
-
-MONTHS = ["January", "February", "March", "April", "May", "June", "July",
-          "August", "September", "October", "November", "December"]
-GENDER_MAP = { "M": "man", "F": "woman" }
-APP_ROOT = "/app"
-CURR_YEAR = datetime.today().year
+import utils
 
 # special cases with extended notes about the early family members
 EXTENDED_NOTES = ["1", "1.1", "1.2", "1.3", "1.5", "1.6", "1.7", "1.8"]
@@ -32,9 +24,9 @@ db = DBConnect()
 
 @app.route('/')
 def index():
-    raw_data_file = get_latest_export()
+    raw_data_file = utils.get_latest_export()
     if raw_data_file is None:
-        raw_data_file = export_data()
+        raw_data_file = utils.export_data(db)
     raw_data_file = "data/" + raw_data_file
     return render_template("index.html", raw_data=raw_data_file)
 
@@ -44,10 +36,10 @@ def search():
     if len(request.args) > 0:
         terms = request.args.get("search", "").split()
         res = db.search_name(terms)
-        res = sorted(res, key=birthdate_sorter)
+        res = sorted(res, key=utils.birthdate_sorter)
         for r in res:
-            r["display_name"] = create_display_name(r)
-            r["life_span"] = create_life_span(r)
+            r["display_name"] = utils.create_display_name(r)
+            r["life_span"] = utils.create_life_span(r)
         return render_template("search_results.html", results=res)
     else:
         return render_template("search_results.html", results=[])
@@ -57,10 +49,10 @@ def search():
 def adv_search():
     if len(request.args) > 0:
         res = db.search_advanced(request.args)
-        res = sorted(res, key=birthdate_sorter)
+        res = sorted(res, key=utils.birthdate_sorter)
         for r in res:
-            r["display_name"] = create_display_name(r)
-            r["life_span"] = create_life_span(r)
+            r["display_name"] = utils.create_display_name(r)
+            r["life_span"] = utils.create_life_span(r)
         return render_template("search_results.html", results=res)
     else:
         return render_template("adv_search.html")
@@ -73,7 +65,7 @@ def person_page(pid):
     if p is None:
         abort(404)
 
-    data["focal"] = format_person_data(p, emphasis=True)
+    data["focal"] = utils.format_person_data(p, emphasis=True)
 
     # get info on focal person's parents
     data["parents"] = []
@@ -83,11 +75,11 @@ def person_page(pid):
     if len(parents) > 0:
         focal_birth_order = None
         for pr in parents:
-            pr_dict = format_person_data(pr)
+            pr_dict = utils.format_person_data(pr)
             parent_ids.append(pr_dict["id"])
             data["parents"].append(pr_dict)
 
-        data["parents"] = sorted(data["parents"], key=birthdate_sorter)
+        data["parents"] = sorted(data["parents"], key=utils.birthdate_sorter)
 
         # get info on focal person's siblings
         if len(parents) == 2:
@@ -98,7 +90,7 @@ def person_page(pid):
                     data["siblings"].append(data["focal"])
                     focal_birth_order = i
                 else:
-                    sib_dict = format_person_data(sib)
+                    sib_dict = utils.format_person_data(sib)
                     data["siblings"].append(sib_dict)
 
     # get info on focal person's spouse(s)
@@ -107,17 +99,17 @@ def person_page(pid):
     for m in marriages:
         marriage = m["marriage"]
 
-        marriage["marriage_date"] = format_date(marriage, "married_day", "married_month", "married_year")
-        if is_attr(marriage, "divorced") and marriage["divorced"]:
-            marriage["divorced_date"] = format_date(marriage, "divorced_day", "divorced_month", "divorced_year")
+        marriage["marriage_date"] = utils.format_date(marriage, "married_day", "married_month", "married_year")
+        if utils.is_attr(marriage, "divorced") and marriage["divorced"]:
+            marriage["divorced_date"] = utils.format_date(marriage, "divorced_day", "divorced_month", "divorced_year")
 
-        spouse = format_person_data(m["spouse"])
+        spouse = utils.format_person_data(m["spouse"])
         marriage["spouse"] = spouse
 
         children = db.get_children(pid, spouse["id"])
         s_children = []
         for c in children:
-            s_children.append(format_person_data(c))
+            s_children.append(utils.format_person_data(c))
 
         marriage["children"] = s_children
         data["marriages"].append(marriage)
@@ -202,9 +194,9 @@ def report():
 
 @app.route('/last-export-date')
 def last_export():
-    raw_data_file = get_latest_export()
+    raw_data_file = utils.get_latest_export()
     if raw_data_file is None:
-        raw_data_file = export_data()
+        raw_data_file = utils.export_data(db)
 
     no_ext = raw_data_file.split(".")[0]
     date = no_ext.split("_")[1]
@@ -229,7 +221,7 @@ def admin_login():
 
                 flash("Logged in successfully.")
                 nexturl = request.args.get("next")
-                if not is_safe_url(nexturl):
+                if not utils.is_safe_url(nexturl):
                     return abort(400)
                 return redirect(nexturl or url_for("admin_index"))
         return render_template("admin/login.html", message="Error: Incorrect username and/or password.", message_style="error")
@@ -248,7 +240,7 @@ def admin_logout():
 def admin_index():
     if len(request.args) > 0 and int(request.args.get("export")) == 1:
         # re-export data from database to CSV file
-        data_fn = export_data()
+        data_fn = utils.export_data(db)
         data_path = url_for("static", filename="data/"+data_fn)
         if data_path.startswith("/"):
             data_path = data_path[1:]
@@ -375,187 +367,9 @@ def admin_editdata():
     return render_template("admin/editdata.html", focal={}, parents={}, marriages={})
 
 
-# Helper functions ---------------------------------------------------
-
-def is_attr(record, attr):
-    if attr in record and record[attr] is not None:
-        return True
-    else:
-        return False
-
-def birthdate_sorter(record):
-    """Function for use in sorted(), to sort birthdates in chronological
-    order. Returns a tuple of (year, month, day).
-    """
-    # in case of unknown values, 1000000 will ensure they are sorted
-    # to the end
-    year, month, day = (1000000, 1000000, 1000000)
-    if "birth_year" in record and record["birth_year"] is not None:
-        try:
-            # if year is integer, great
-            year = int(record["birth_year"])
-        except ValueError:
-            try:
-                # this should handle cases like "1945?" and
-                # "1945 or 1946"
-                year = int(record["birth_year"][0:4])
-            except:
-                pass
-
-    if "birth_month" in record and record["birth_month"] is not None:
-        try:
-            month = MONTHS.index(record["birth_month"]) + 1
-        except:
-            pass
-    
-    if "birth_day" in record and record["birth_day"] is not None:
-        try:
-            day = int(record["birth_day"])
-        except ValueError:
-            try:
-                day = int(record["birth_day"][0:2])
-            except:
-                pass
-    return (year, month, day)
-
-def format_person_data(record, emphasis=False):
-    output = dict(record)
-    output["display_name"] = create_display_name(record)
-    output["life_span"] = create_life_span(record)
-
-    output["birth_date"] = format_date(record, "birth_day", "birth_month", "birth_year", all_blanks=True)
-    if is_attr(output, "birth_year") and record["birth_year"].isnumeric() and int(record["birth_year"]) >= (CURR_YEAR-100):
-        output["death_date"] = format_date(record, "death_day", "death_month", "death_year")
-        # assume the best if someone is less than 100 years old :)
-    else:
-        output["death_date"] = format_date(record, "death_day", "death_month", "death_year", all_blanks=True)
-
-    # used in graphical tree
-    output["name"] = create_short_name(record)
-    if is_attr(output, "gender") and output["gender"] in GENDER_MAP:
-        output["class"] = GENDER_MAP[output["gender"]]
-    output["extra"] = { "url": url_for("person_page", pid=output["id"]) }
-    
-    if emphasis:
-        output["textClass"] = "emphasis"
-    return output
-
-def create_display_name(record):
-    name = ""
-    if is_attr(record, "first_name"):
-        if record["first_name"] == "Unnamed":
-            return record["first_name"]
-        name += record["first_name"]
-    else:
-        name += "_____"
-    if is_attr(record, "nickname"):
-        name += f" ({record['nickname']})"
-    if is_attr(record, "middle_name1"):
-        if record["pref_name"] == "M1":
-            name += f" <u>{record['middle_name1']}</u>"
-        else:
-            name += " " + record['middle_name1']
-    if is_attr(record, "middle_name2"):
-        if record["pref_name"] == "M2":
-            name += f" <u>{record['middle_name2']}</u>"
-        else:
-            name += " " + record['middle_name2']
-    if is_attr(record, "last_name"):
-        name += " " + record["last_name"]
-    else:
-        name += " _____"
-    return name
-
-def create_short_name(record):
-    name = ""
-    if is_attr(record, "first_name"):
-        if record["first_name"] == "Unnamed":
-            return record["first_name"]
-        name += record["first_name"]
-    else:
-        name += "_____"
-    if is_attr(record, "last_name"):
-        name += " " + record["last_name"]
-    else:
-        name += " _____"
-    return name
-
-def create_life_span(record):
-    string = ""
-    if is_attr(record, "birth_year"):
-        string += f" ({record['birth_year']}"
-    else:
-        string += " (? "
-
-    if is_attr(record, "death_year"):
-        string += f" - {record['death_year']})"
-    elif is_attr(record, "birth_year") and record["birth_year"].isnumeric() and int(record["birth_year"]) >= (CURR_YEAR-100):
-        string += " - present)"
-        # assume the best if someone is less than 100 years old :)
-    else:
-        string += " - ?)"
-    return string
-
-def format_date(record, day, month, year, all_blanks=False):
-    string = ""
-    if is_attr(record, day):
-        if is_attr(record, month):
-            string += f"{record[month]} {record[day]}"
-        else:
-            string += f"_____ {record[day]}"
-    elif is_attr(record, month):
-        if all_blanks:
-            string += f"{record[month]} __"
-        else:
-            string += str(record[month])
-    elif all_blanks:
-        string += "_____ __"
-
-    if is_attr(record, year):
-        if string != "":
-            string += ", " + str(record[year])
-        else:
-            string += str(record[year])
-    elif all_blanks:
-        string += ", ____"
-
-    if string == "":
-        string = None
-    return string
-
-def get_latest_export():
-    try:
-        files = os.listdir(os.path.join(APP_ROOT, "static/data"))
-        files = [f for f in files if f.endswith(".zip")]
-        if len(files) > 0:
-            files = sorted(files)
-            return files[-1]
-        else:
-            return None
-    except FileNotFoundError:
-        return None
-
-def export_data():
-    date = datetime.now().strftime("%Y%m%d")
-    tables = ["people", "marriages", "children"]
-    with tempfile.TemporaryDirectory() as tmpdir:
-        for table in tables:
-            with open(os.path.join(tmpdir, f"{table}_{date}.csv"), "w") as f:
-                db.export_data(table, f)
-
-        os.makedirs(os.path.join(APP_ROOT, "static/data"), exist_ok=True)
-        shutil.make_archive(os.path.join(APP_ROOT, "static/data", f"data_{date}"), "zip", tmpdir)
-    return f"data_{date}.zip"
-
 @login_manager.user_loader
 def load_user(user_id):
     return User.get(user_id)
-
-def is_safe_url(target):
-    ref_url = urlparse(request.host_url)
-    test_url = urlparse(urljoin(request.host_url, target))
-    return test_url.scheme in ('http', 'https') and \
-        ref_url.netloc == test_url.netloc
 
 
 if __name__ == "__main__":
---
+++ app/app/utils.py
@@ -0,0 +1,188 @@
+from datetime import datetime
+import os
+import shutil
+import tempfile
+from urllib.parse import urlparse, urljoin
+
+from flask import request, url_for
+
+MONTHS = ["January", "February", "March", "April", "May", "June", "July",
+          "August", "September", "October", "November", "December"]
+GENDER_MAP = { "M": "man", "F": "woman" }
+CURR_YEAR = datetime.today().year
+
+def is_attr(record, attr):
+    if attr in record and record[attr] is not None:
+        return True
+    else:
+        return False
+
+def birthdate_sorter(record):
+    """Function for use in sorted(), to sort birthdates in chronological
+    order. Returns a tuple of (year, month, day).
+    """
+    # in case of unknown values, 1000000 will ensure they are sorted
+    # to the end
+    year, month, day = (1000000, 1000000, 1000000)
+    if "birth_year" in record and record["birth_year"] is not None:
+        try:
+            # if year is integer, great
+            year = int(record["birth_year"])
+        except ValueError:
+            try:
+                # this should handle cases like "1945?" and
+                # "1945 or 1946"
+                year = int(record["birth_year"][0:4])
+            except:
+                pass
+
+    if "birth_month" in record and record["birth_month"] is not None:
+        try:
+            month = MONTHS.index(record["birth_month"]) + 1
+        except:
+            pass
+    
+    if "birth_day" in record and record["birth_day"] is not None:
+        try:
+            day = int(record["birth_day"])
+        except ValueError:
+            try:
+                day = int(record["birth_day"][0:2])
+            except:
+                pass
+    return (year, month, day)
+
+def format_person_data(record, emphasis=False):
+    output = dict(record)
+    output["display_name"] = create_display_name(record)
+    output["life_span"] = create_life_span(record)
+
+    output["birth_date"] = format_date(record, "birth_day", "birth_month", "birth_year", all_blanks=True)
+    if is_attr(output, "birth_year") and record["birth_year"].isnumeric() and int(record["birth_year"]) >= (CURR_YEAR-100):
+        output["death_date"] = format_date(record, "death_day", "death_month", "death_year")
+        # assume the best if someone is less than 100 years old :)
+    else:
+        output["death_date"] = format_date(record, "death_day", "death_month", "death_year", all_blanks=True)
+
+    # used in graphical tree
+    output["name"] = create_short_name(record)
+    if is_attr(output, "gender") and output["gender"] in GENDER_MAP:
+        output["class"] = GENDER_MAP[output["gender"]]
+    output["extra"] = { "url": url_for("person_page", pid=output["id"]) }
+    
+    if emphasis:
+        output["textClass"] = "emphasis"
+    return output
+
+def create_display_name(record):
+    name = ""
+    if is_attr(record, "first_name"):
+        if record["first_name"] == "Unnamed":
+            return record["first_name"]
+        name += record["first_name"]
+    else:
+        name += "_____"
+    if is_attr(record, "nickname"):
+        name += f" ({record['nickname']})"
+    if is_attr(record, "middle_name1"):
+        if record["pref_name"] == "M1":
+            name += f" <u>{record['middle_name1']}</u>"
+        else:
+            name += " " + record['middle_name1']
+    if is_attr(record, "middle_name2"):
+        if record["pref_name"] == "M2":
+            name += f" <u>{record['middle_name2']}</u>"
+        else:
+            name += " " + record['middle_name2']
+    if is_attr(record, "last_name"):
+        name += " " + record["last_name"]
+    else:
+        name += " _____"
+    return name
+
+def create_short_name(record):
+    name = ""
+    if is_attr(record, "first_name"):
+        if record["first_name"] == "Unnamed":
+            return record["first_name"]
+        name += record["first_name"]
+    else:
+        name += "_____"
+    if is_attr(record, "last_name"):
+        name += " " + record["last_name"]
+    else:
+        name += " _____"
+    return name
+
+def create_life_span(record):
+    string = ""
+    if is_attr(record, "birth_year"):
+        string += f" ({record['birth_year']}"
+    else:
+        string += " (? "
+
+    if is_attr(record, "death_year"):
+        string += f" - {record['death_year']})"
+    elif is_attr(record, "birth_year") and record["birth_year"].isnumeric() and int(record["birth_year"]) >= (CURR_YEAR-100):
+        string += " - present)"
+        # assume the best if someone is less than 100 years old :)
+    else:
+        string += " - ?)"
+    return string
+
+def format_date(record, day, month, year, all_blanks=False):
+    string = ""
+    if is_attr(record, day):
+        if is_attr(record, month):
+            string += f"{record[month]} {record[day]}"
+        else:
+            string += f"_____ {record[day]}"
+    elif is_attr(record, month):
+        if all_blanks:
+            string += f"{record[month]} __"
+        else:
+            string += str(record[month])
+    elif all_blanks:
+        string += "_____ __"
+
+    if is_attr(record, year):
+        if string != "":
+            string += ", " + str(record[year])
+        else:
+            string += str(record[year])
+    elif all_blanks:
+        string += ", ____"
+
+    if string == "":
+        string = None
+    return string
+
+def get_latest_export():
+    try:
+        files = os.listdir(os.path.join(os.environ.get("APP_ROOT"), "static/data"))
+        files = [f for f in files if f.endswith(".zip")]
+        if len(files) > 0:
+            files = sorted(files)
+            return files[-1]
+        else:
+            return None
+    except FileNotFoundError:
+        return None
+
+def export_data(db_conn):
+    date = datetime.now().strftime("%Y%m%d")
+    tables = ["people", "marriages", "children"]
+    with tempfile.TemporaryDirectory() as tmpdir:
+        for table in tables:
+            with open(os.path.join(tmpdir, f"{table}_{date}.csv"), "w") as f:
+                db_conn.export_data(table, f)
+
+        os.makedirs(os.path.join(os.environ.get("APP_ROOT"), "static/data"), exist_ok=True)
+        shutil.make_archive(os.path.join(os.environ.get("APP_ROOT"), "static/data", f"data_{date}"), "zip", tmpdir)
+    return f"data_{date}.zip"
+
+def is_safe_url(target):
+    ref_url = urlparse(request.host_url)
+    test_url = urlparse(urljoin(request.host_url, target))
+    return test_url.scheme in ('http', 'https') and \
+        ref_url.netloc == test_url.netloc
