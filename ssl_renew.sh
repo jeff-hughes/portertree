7def79a37cf0d7cbd2e1b428764d84fe9b4339fc
Jeff Hughes <jeff.hughes@gmail.com>
2021-10-03 19:27:18 -0400

    Echo date when ssl certs are renewed

--- ssl_renew.sh
+++ ssl_renew.sh
@@ -4,5 +4,6 @@ COMPOSE="/usr/local/bin/docker-compose -f compose.common.yml -f compose.prod.yml
 DOCKER="/usr/bin/docker"
 
 cd /home/jeff/portertree/
+echo $(date)
 $COMPOSE run certbot renew && $COMPOSE exec -d -T app nginx -s reload
 $DOCKER system prune -f
